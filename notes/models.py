b31ed70f82cf6466218870c6e59c4ff6d0091a87
Sadman Hossain Ridoy <turjoridoy@gmail.com>
2023-04-21 19:54:36 +0600

    note model created

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
 # Create your models here.
+from django.utils import timezone
+
+
+class Note(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=64, null=True, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ModelViewSet
+
+from notes.models import Note
+from notes.serializer import NoteSerializer
+
+
+class NoteViewSet(ModelViewSet):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+    permission_classes = [AllowAny]
+
