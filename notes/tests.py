d2fa149f1d76a8c5c91c63192aeffafaab5d6d69
Sadman Hossain Ridoy <turjoridoy@gmail.com>
2023-04-25 17:59:59 +0600

    unit test

--- notes/tests.py
+++ notes/tests.py
@@ -13,15 +13,17 @@ class NoteTestCase(TestCase):
 
     def test_note_that_exists(self):
         response = Note.objects.filter(title='Note44', content='a')
-        self.assertEqual(response, 'The note already exists')
+        self.assertEqual(len(response), 0)
 
     def test_user_that_exists(self):
-        user = User.objects.create(username="admin77", password="1234")
-        self.assertEqual(user, 'This user already exists')
+        user = User.objects.create(username="admin7887", password="1234")
+        self.assertEqual(user.username, "admin7887")
 
     def test_login(self):
-        user = User.objects.get(username="admin77", password="13234")
-        self.assertEqual(user, 'Login failed')
+        user = User.objects.filter(username="admin77", password="13234")
+
+        # Checking this user already exists or not
+        self.assertEqual(len(user), 0)
 
 
 
